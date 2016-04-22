7936c8f7b8edde17747d9212e60deb4b05a8b2b2
fengbaoli <wenyefbl@163.com>
2016-04-22 23:36:01 +0800

    Added files via upload

---
+++ sftp.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+__author__ = 'blfeng'
+import  paramiko,os,Sftp,string
+
+def main():
+    hostname = ''
+    username = ''
+    password = ''
+    remotepath = '/tmp'
+    localpath = 'lib'
+    port = 22
+    f=open("conf/hostname.txt")
+    while True:
+        line = f.readline()
+        if line:
+            ini_line = line.split(':',3)
+            c = map(string.strip, ini_line)
+            hostname=c[0]
+            username=c[1]
+            password=c[2]
+            s = Sftp(hostname=hostname,username=username,password= password,port=port,localpath=localpath,remotepath=remotepath)
+            s.sftp()
+            
+        else:
+            break
+
+if __name__=='__main__':
+    main()
